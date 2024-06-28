a322899fa5931df1697feafb91e386df150a1a8e
Kage Kowalski <kagekowalski@gmail.com>
2024-06-28 10:28:59 -0700

    EnemyFactory Fix
    EnemyFactory no longer causes the game to crash on Play

--- msc/enemy_factory.gd
+++ msc/enemy_factory.gd
@@ -5,9 +5,9 @@ extends Node2D
 
 
 @onready var start_node_count = self.get_child_count()
-@onready var current_node_count = self.get_child_count()
+@onready var current_node_count: int
 @onready var enemies := [SimpleEnemy]
-@onready var current_wave := 1
+@onready var current_wave := 0
 @onready var points: int
 @onready var spawn_wait_interval: float
 @onready var rng := RandomNumberGenerator.new()
@@ -15,19 +15,28 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	while _game_is_active():
+	$WaveTimer.start(5.0)
+
+
+func _on_wave_timer_timeout():
+	current_node_count = get_child_count()
+	if start_node_count == current_node_count and _game_is_active():
+		current_wave += 1
 		points = ceili(pow(current_wave, 1.1) + (0.5 * current_wave))
 		spawn_wait_interval = (4.9 / 0.9) * pow((0.9), current_wave) + 0.10
-		while points > 0:
-			var spawn_point := _get_spawn_point()
-			var enemy := _purchase_enemy()
-			_spawn_enemy(enemy, spawn_point)
-			await get_tree().create_timer(spawn_wait_interval)
-		current_node_count = self.get_child_count()
-		while start_node_count != current_node_count and _game_is_active():
-			current_node_count = self.get_child_count()
-			await get_tree().create_timer(1)
-		current_wave += 1
+		$SpawnTimer.start()
+	else:
+		$WaveTimer.start(5.0)
+
+
+func _on_spawn_timer_timeout():
+	if points > 0:
+		var spawn_point := _get_spawn_point()
+		var enemy := _purchase_enemy()
+		_spawn_enemy(enemy, spawn_point)
+		$SpawnTimer.start(spawn_wait_interval)
+	else:
+		$WaveTimer.start(5.0)
 
 
 func _game_is_active() -> bool:
